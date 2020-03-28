b660e374175a6e966291589f16103c3de445f017
Isidro Morla <euris110811@gmail.com>
2020-03-27 22:20:05 -0400

    The principal files has been added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// The data of cats
+var model = {
+
+}
+
+// My cat-clicker's octopus
+var controller = {
+
+}
+
+// List of cats
+var view1 = {
+
+}
+
+// Current cat in the view1
+var view2 = {
+
+}
